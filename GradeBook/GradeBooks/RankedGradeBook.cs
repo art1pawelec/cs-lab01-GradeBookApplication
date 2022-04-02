0cba3fb0c7a9795e31bb669540c655a745069091
art1pawelec <80786313+art1pawelec@users.noreply.github.com>
2022-04-02 15:15:59 +0200

    Creating the RankedGradeBook class

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook
+    {
+        public class RankedGradeBook : BaseGradeBook
+        {
+
+        }
+    }
+}
