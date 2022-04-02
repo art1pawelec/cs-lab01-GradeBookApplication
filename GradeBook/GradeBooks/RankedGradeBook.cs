963880c6c5b1583d8b24aa91e35ebd1ac57e3b1d
art1pawelec <80786313+art1pawelec@users.noreply.github.com>
2022-04-02 17:11:40 +0200

    Update CreateCommand's Conditions

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -30,7 +30,7 @@ namespace GradeBook.GradeBooks
             else
             base.CalculateStatistics();
         }
-        public override void CalculateStudentStatistics()
+        public override void CalculateStudentStatistics(string name)
         {
             if (Students.Count < 5)
                 Console.WriteLine("Ranked grading requires at least 5 students.");
