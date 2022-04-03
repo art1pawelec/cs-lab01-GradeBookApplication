4f44b6ba1dc1014d318cef03dcac6c005656865f
art1pawelec <80786313+art1pawelec@users.noreply.github.com>
2022-04-03 12:49:17 +0200

    Update IsWeighted to BaseGradeBook

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -20,6 +20,7 @@ namespace GradeBook.GradeBooks
         {
             Name = name;
             Students = new List<Student>();
+            this.IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
